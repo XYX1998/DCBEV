80561fa54939b0154670371ddb267bbd9494dd92
XYX1998 <52532972+XYX1998@users.noreply.github.com>
2025-07-10 16:26:04 +0800

    Delete models directory

--- models/backbones/__init__.py
+++
@@ -1,5 +0,0 @@
-from .resnet import ResNet, ResNetV1c, ResNetV1d
-from .swin import SwinTransformer
-__all__ = [
-    'ResNet', 'ResNetV1c', 'ResNetV1d', 'SwinTransformer'
-]
--- models/decode_heads/__init__.py
+++
@@ -1,8 +0,0 @@
-from .pyramid_head import PyramidHead
-from .pyramid_head_argoverse import PyramidHeadArgoverse
-from .pyramid_head_kitti import PyramidHeadKitti
-from .Polarhead import Polarhead
-from .pyramid_head_with_rl import PyramidHead_withRL
-__all__ = [
-    'PyramidHead','PyramidHeadArgoverse','PyramidHeadKitti','Polarhead','pyramid_head_with_rl'
-]
